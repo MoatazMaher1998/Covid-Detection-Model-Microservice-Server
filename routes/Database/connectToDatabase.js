4ff7a9c05ae551f2d9558cf240bfd20b178e9bf1
Moataz Maher <Moatazmaher1998@gmail.com>
2021-01-24 00:08:53 +0200

    Duplicate emails solved

--- routes/Database/connectToDatabase.js
+++ routes/Database/connectToDatabase.js
@@ -12,28 +12,34 @@ const userSchema = new mongoose.Schema({
 });
 var User = mongoose.model("User", userSchema);
 function addUser(data,res){
-    if(data.password != data.confirmPassword){
-      res.status(401).json("Passwords does not match");
-    }
-    else
-    bcrypt.hash(data.password, parseInt(process.env.BCRYPT_SALT_ROUNDS,10))
-    .then(function(hashedPassword) {
-        data.password = hashedPassword;
-        var myData = new User({
-          name: data.fullname,
-          email: data.email,
-          password: data.password,
-          age: data.age,
-          gender: data.gender
-        });
-        myData.save().then(item => {
-          res.send("item saved to database");
-        }).catch(err => {
-         res.status(400).send("unable to save to database");
-        });
+    User.find({'email' : data.email},function(err,user){
+      if(err){console.log(err);}
+      else if(user.length !== 0){
+        console.log("duplicate");
+       return res.status(401).json("user already exist");
+      }
+      else if(data.password !== data.confirmPassword){
+        res.status(401).json("Passwords does not match");
+      }
+      else 
+      bcrypt.hash(data.password, parseInt(process.env.BCRYPT_SALT_ROUNDS,10))
+      .then(function(hashedPassword) {
+          data.password = hashedPassword;
+          var myData = new User({
+            name: data.fullname,
+            email: data.email,
+            password: data.password,
+            age: data.age,
+            gender: data.gender
+          });
+          myData.save().then(item => {
+            res.status(201).send("user added saved to database");
+          }).catch(err => {
+           res.status(400).send("unable to save to database");
+          });
+      })
     })
     
-   
    
 }
 function startConnection(type){
