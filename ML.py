45906a472dee4dc2daa9413b21160e9c9b2c2dea
Moataz Maher <moatazmaher1998@gmail.com>
2021-01-25 16:24:14 +0200

    Update ML.py

--- ML.py
+++ ML.py
@@ -48,7 +48,7 @@ def INCV3():
 model = INCV3()
 model.load_weights('./bestmodelinception.h5')
 response = requests.get(sys.argv[1])
-
+print("im in python now")
 img = np.array(Image.open(io.BytesIO(response.content))) / 255
 #print("IMG" , img.shape)
 
