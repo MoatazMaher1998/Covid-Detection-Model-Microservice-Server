34f0737c1885aba3551ca6531dcbf1c63f45c604
Moataz Maher <moatazmaher1998@gmail.com>
2021-01-25 18:02:19 +0200

    Update Corona.py

--- Corona.py
+++ Corona.py
@@ -5,8 +5,4 @@ from PIL import Image
 import PIL
 # creating a image object (main image)
 print(sys.argv)
-im1 = Image.open(sys.argv[1])
-print(im1)
-# save a image using extension
-im1 = im1.save("geeks.png")
 print("Output from Python")
